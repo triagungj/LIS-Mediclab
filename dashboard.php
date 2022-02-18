715e36e8821d273348a49fa6ea5a0adfc17c1ddb
Tri Agung J <triagungj@gmail.com>
2022-02-19 02:10:52 +0700

    initiate project

---
+++ dashboard.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<?php
+include 'config.php';
+
+error_reporting(0);
+
+session_start();
+
+if (isset($_SESSION['username'])) {
+    $username = $_SESSION['username'];
+
+    $sql = "SELECT * FROM users WHERE username='$username'";
+    $result = mysqli_query($conn, $sql);
+    if ($result->num_rows > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $name = $row['name'];
+    }
+} else {
+    header("Location: ./");
+}
+
+?>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <title>Mediclab - Dashboard</title>
+</head>
+
+<body>
+    <div class="header bg-primary">
+        <div class="container d-flex justify-content-between pt-4 pb-2">
+            <h3 class="text-light"><b>MEDICLAB</b></h3>
+            <div class="d-flex align-items-start">
+                <h5 class="mr-4 text-light">Selamat pagi, <?= $name ?></h5>
+                <div style="min-width:40px"></div>
+                <a href="logout.php"><button class="btn btn-light">Logout</button></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="content-dashboard container mt-4">
+        <div class="row menu-items g-2">
+            <div class="col-lg-6 col-12 chart">
+                <div class="p-3 bg-light text-center">
+                    <img src="assets/bar_chart_black_24dp.svg" style="height: 280px; width: 280px;" alt="">
+                    <h2>CHART</h2>
+                </div>
+            </div>
+            <div class="col-lg-6 col-12 container menus">
+                <div class="row">
+                    <div class="col-lg-6 col-12 bg-danger text-center">
+                        <img src="assets/fact_check_black_24dp.svg" alt="" class="image-worklist">
+                        <h6 class=" text-light ">WORKLIST</h6>
+                    </div>
+                    <div class=" col-lg-6 col-12">
+                        REPORT
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+
+<script src="js/bootstrap.bundle.min.js"></script>
+
+</html>
---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+
+include 'config.php';
+
+error_reporting(0);
+
+session_start();
+
+if (isset($_SESSION['username'])) {
+    header("Location: dashboard.php");
+}
+
+if (isset($_POST['submit'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $result = mysqli_query($conn, $sql);
+    if ($result->num_rows > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $_SESSION['username'] = $row['username'];
+        header("Location: dashboard.php");
+    } else {
+        echo "<script>alert('Username atau password Anda salah. Silahkan coba lagi!')</script>";
+    }
+}
+
+?>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <title>Mediclab - Login</title>
+</head>
+
+<body class="bg-primary body-login">
+
+    <div>
+        <div class="container pt-4 pb-2">
+            <h4><b class="text-white">MEDICLAB</b></h4>
+        </div>
+        <div class="form-login align-center bg-light">
+            <form action="" method="POST">
+                <h4 class="text-primary"><b>Login</b></h4>
+                <div class="mt-4">
+                    <input type="username" name="username" class="form-control" id="inputUsername" placeholder="Username" required>
+                </div>
+                <div class="mt-2">
+                    <input type="password" name="password" class=" form-control" id="inputPassword" placeholder="Password" required>
+                </div>
+                <div class="mt-4 text-end">
+                    <button name="submit" class="btn btn-primary">Login</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
