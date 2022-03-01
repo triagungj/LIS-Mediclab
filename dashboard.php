0b3623a2c7f6fb7367c9e1b9b2fcd3ba5279a518
Tri Agung J <triagungj@gmail.com>
2022-03-01 23:48:33 +0700

    worklist UI

--- dashboard.php
+++ dashboard.php
@@ -24,7 +24,6 @@ if (isset($_SESSION['username'])) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <link href="css/style-main.css" rel="stylesheet">
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <title>Mediclab - Dashboard</title>
@@ -34,13 +33,14 @@ if (isset($_SESSION['username'])) {
     <div class="header bg-primary">
         <div class="container d-flex justify-content-between pt-4 pb-2">
             <h3 class="text-light"><b>MEDICLAB</b></h3>
-            <div class="d-flex align-items-start">
+            <div class="d-flex align-items-end">
                 <h5 class="mr-4 text-light">Selamat pagi, <?= $name ?></h5>
                 <div style="min-width:40px"></div>
                 <a href="logout.php"><button class="btn btn-light">Logout</button></a>
             </div>
         </div>
     </div>
+    <div style="height: 60px;"></div>
 
     <div class="content-dashboard container mt-4">
         <div class="row menu-items g-2">
@@ -54,7 +54,7 @@ if (isset($_SESSION['username'])) {
                 <div class="row">
                     <div class="col-lg-1"></div>
                     <div class="col-lg-5 col-12 text-center">
-                        <a href="#" class="decoration-none">
+                        <a href="worklist.php" class="decoration-none">
                             <div class="bg-danger pt-2 pb-2 border-radius-4">
                                 <img src="assets/fact_check_black_24dp.svg" alt="image" class="filter-white">
                                 <div style="min-height: 15px;"></div>
